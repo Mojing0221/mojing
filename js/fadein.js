307ae1bfa3e2a849b9c69d7a21c35952c0311fed
Mojing <mojing0221@gmail.com>
2026-06-02 22:14:28 +0800

    排版

--- js/fadein.js
+++ js/fadein.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const observerOptions = {
     root: null,
     rootMargin: '0px',
-    threshold: 0.15 
+    threshold: 0.1
   };
 
   const fadeObserver = new IntersectionObserver((entries, observer) => {
